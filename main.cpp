7a0855db4fa17eef313691cf4468453c96c1638a
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-08-31 10:47:01 -0500

    COP3014 2nd programming assignment student project template

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//
+// COP3014 - 2nd programming assignment "Fast Food Can be Expensive"
+
+/*** Don't change any text below */
+// Important announcement on Significance of Academic Honor Policy
+// The  Academic Honor Policy is an important aspect of keeping the integrity
+// and quality of the computer science courses in general. Please don't share
+// your project source with any other students on any occasion. I would like
+// to let you know that, for the summer 2018 COP3014 course, we found several
+// students violated the Academic Honor Policy by sharing their programming
+// assignment source in an advertent and inadvertent manners. Please note that
+// we will run MOSS" (plagiarism detection software tool) on future COP3014 student
+// project submissions together with your summer 2018 project" submissions.
+// In case your project source code happens to be >%75" similar to future student
+// project source (which is statistically" almost impossible), your grade will
+// be invalidated and your academic honor code violation will be reported to Dean's
+// office. The following is the quick reference for Academic Honor Policy:"
+// https://fda.fsu.edu/sites/g/files/upcbnu636/files/Media/Files/Academic%20Honor%20Policy/AHP%20Quick%20Reference%20Guide%20Nov%202017.pdf
+//
+
+/*** Add your comments starting from here ***/
+
+int main() {
+    // Add your implementation here!
+
+    return 0;
+}
